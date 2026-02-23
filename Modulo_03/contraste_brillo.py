a65b4f53390dddedc5dae88d81096bd0075b3d97
Ruben Hernandez <rubendez21@gmail.com>
2026-02-23 09:03:44 -0600

    Ajuste de brillo y contraste
    Signed-off-by: Ruben Hernandez <rubendez21@gmail.com>

---
+++ Modulo_03/contraste_brillo.py
@@ -0,0 +1,131 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
+import numpy as np
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
+class Ventana(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Control de contraste y brillo"
+        self.geometry("1100x500")
+        self.protocol("WM_DELETE_WINDOW", self.cierre)
+
+        self.ruta_imagen = "Modulo_03/img/lena.jpeg"
+        
+        self.imagen = cv2.imread(self.ruta_imagen)
+        self.imagen_pl = Image.open(self.ruta_imagen)
+        self.imagen_tk = ImageTk.PhotoImage(self.imagen_pl)
+        self.imagen_procesada = cv2.imread(self.ruta_imagen)
+        self.imagen_procesada_pl = Image.open(self.ruta_imagen)
+        self.imagen_procesada_tk = ImageTk.PhotoImage(self.imagen_procesada_pl)
+
+        self.fig, self.ax = plt.subplots(figsize=(1, 2))
+
+    def cierre(self):
+        print("Cerrando ventanas")
+        plt.close("all")
+        self.quit()
+        self.destroy()
+
+    def mostrar_ventana(self):
+        self.__canvas_imagen = tk.Canvas(self, width=250, height=250, background="black")
+        self.__canvas_imagen_proc = tk.Canvas(self, width=250, height=250, background="black")
+
+        if self.imagen is not None:
+            self.__canvas_imagen.create_image(250/2, 250/2, image=self.imagen_tk)
+            self.__canvas_imagen.place(x=30, y=40)
+
+        self.mostrar_imagen_procesada(0, 1, 1)
+
+        self.__escala_brillo = tk.Scale(self, label="Brillo", from_=-100, to=100, orient=tk.HORIZONTAL, command=self.actualizar_imagen)
+        self.__escala_contraste = tk.Scale(self, label="Contraste", from_=-1, to=3, orient=tk.HORIZONTAL, resolution=0.05, command=self.actualizar_imagen)
+        self.__escala_gamma = tk.Scale(self, label="Gamma", from_=-2, to=4, orient=tk.HORIZONTAL, resolution=0.05, command=self.actualizar_imagen)
+        self.__escala_contraste.set(1) 
+        self.__escala_gamma.set(1)
+        
+        self.__escala_brillo.place(x=100, y=250+50)
+        self.__escala_contraste.place(x=250 , y=250+50)
+        self.__escala_gamma.place(x=100, y=250+120)
+
+        self.__canvas_hist = FigureCanvasTkAgg(self.fig, master=self)
+        self.__canvas_hist.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.__canvas_hist.get_tk_widget().place(x=550, y=40, width=500, height=400)
+        self.mostrar_histograma(self.obtener_v(self.imagen_procesada))
+
+    def mostrar_imagen_procesada(self, brillo, contraste, gamma):
+        self.procesar_imagen(contraste, brillo, gamma)
+        im = cv2.cvtColor(self.imagen_procesada, cv2.COLOR_BGR2RGB)
+        self.imagen_procesada_pl = Image.fromarray(im)
+        self.imagen_procesada_tk = ImageTk.PhotoImage(self.imagen_procesada_pl)
+        self.__canvas_imagen_proc.create_image(250/2, 250/2, image=self.imagen_procesada_tk)
+        self.__canvas_imagen_proc.place(x=250 + 30, y=40)
+        
+    def actualizar_imagen(self, val):
+        brillo = self.__escala_brillo.get()
+        contraste = self.__escala_contraste.get()
+        gamma = self.__escala_gamma.get()
+        self.mostrar_imagen_procesada(brillo, contraste, gamma)
+        self.mostrar_histograma(self.obtener_v(self.imagen_procesada))
+        self.__canvas_hist.draw()
+
+    def obtener_v(self, imagen):
+        return cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)[:,:,2]
+
+    def mostrar_histograma(self, datos):
+        self.ax.clear()
+        self.ax.set_title('Histograma de Imagen Procesada')
+        self.ax.set_xlabel('Valor')
+        self.ax.set_ylabel('Frecuencia')
+        self.ax.hist(datos.flatten(), bins=256, alpha=0.7, range=(0,255), color='blue', edgecolor='black')
+        self.__canvas_hist.draw_idle()
+        
+
+    def procesar_imagen(self, contraste, brillo, gamma):
+        imagen_color_HSV = cv2.cvtColor(self.imagen, cv2.COLOR_BGR2HSV)
+        imagen_v = imagen_color_HSV[:,:,2]
+        imagen_color_HSV_procesada = Procesador.contraste_brillo_centrado(imagen_v, contraste, brillo)
+        imagen_color_HSV_procesada = Procesador.correccion_gamma(imagen_color_HSV_procesada, gamma)
+        imagen_color_HSV[:,:,2] = imagen_color_HSV_procesada
+        self.imagen_procesada =  cv2.cvtColor(imagen_color_HSV, cv2.COLOR_HSV2BGR)
+
+class Procesador:
+    @staticmethod
+    def contraste_brillo(imagen, contraste, brillo):
+        imagen_p = imagen.astype(np.float32)    
+        mat_proc = np.clip(contraste * imagen_p + brillo, 0, 255).astype(np.uint8)
+        return mat_proc
+    
+    @staticmethod
+    def contraste_brillo_centrado(imagen, contraste, brillo):
+        imagen_p = imagen.astype(np.float32)    
+        mat_proc = np.clip(contraste * (imagen_p - 128) + 128 + brillo, 0, 255).astype(np.uint8)
+        return mat_proc
+    
+    @staticmethod
+    def correccion_gamma(imagen, gamma):
+        """
+        ¿Qué hace gamma visualmente?
+        γ < 1
+        Aclara sombras
+        Expande tonos oscuros
+        Comprime altas luces
+        Muy útil para imágenes oscuras.
+        γ > 1
+        Oscurece sombras
+        Expande altas luces
+        Comprime tonos bajos
+        Útil para imágenes muy brillantes.
+        """
+        return np.clip(((imagen.astype(np.float32)*1/255)**(gamma))*255, 0, 255).astype(np.uint8)
+
+
+if __name__ == "__main__":
+    v = Ventana()
+
+    v.mostrar_ventana()
+
+    v.mainloop()
